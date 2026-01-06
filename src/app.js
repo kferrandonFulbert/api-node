728303b9f1b522eb3c1ee1c0cc1084197571c549
KferrandonFulbert <98470782+kferrandonFulbert@users.noreply.github.com>
2026-01-06 11:37:30 +0100

    add rate limit

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js';
 import { pinoMiddleware } from './utils/logger.js';
+import { rateLimit } from 'express-rate-limit'
 
 
 
@@ -13,6 +14,14 @@ const app = express();
 
 const API_VERSION = process.env.API_VERSION || 'v1';
 const API_PREFIX = `/api/${API_VERSION}`;
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+})
+app.use(limiter);
 
 app.use(cors());
 app.use(pinoMiddleware);
